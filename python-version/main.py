1f3a38eb6834ace40649080f155e9db75f199d4b
Michael Lugalia <michael@Michaels-MacBook-Air.local>
2026-05-30 15:33:50 -0400

    Added a goal function and list.

--- python-version/main.py
+++ python-version/main.py
@@ -12,13 +12,26 @@ def menu(name):
 
     choice = input("\nEnter your choice: ")
 
-    print(f"\nYou selected option {choice}")
+    if choice == "1":
+        add_goal()
 
 
 def main():
     name = input("Name: ")
     menu(name)
+    print(goals)
 
+goals = [] #This will store all the goals and their respective information.
+
+def add_goal(): #This function will add a goal to the goal list.
+    goal = {}
+    goal["Name"] = input("What is your goal?: ")
+    goal["Deadline"] = input("When would you like to finish it?: ")
+    goal["Status"] = "Started"
+    goals.append(goal)
+    print("\nGoal added successfully!")
+
+    
 
 main()
 
